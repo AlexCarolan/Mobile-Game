4cd42599b27f85a4152b5a30196b8bbd0a5e1c4d
Alex Carolan <alexcarolan@alexs-mbp.home>
2020-11-15 17:48:34 +0000

    added simple player physics

---
+++ Mobile Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidBody.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
