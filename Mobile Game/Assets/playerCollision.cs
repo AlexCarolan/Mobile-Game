55f9ac3299246e45b38331aa3dd524c4a18b5514
Alex Carolan <alexcarolan@alexs-mbp.home>
2020-12-30 16:26:45 +0000

    added collision

---
+++ Mobile Game/Assets/playerCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+    private PlayerMovement playerMovement;
+    private PlayerCameraTether playerCameraTether;
+
+    private void Start()
+    {
+        playerMovement = this.GetComponent<PlayerMovement>();
+        playerCameraTether = GameObject.FindGameObjectsWithTag("MainCamera")[0].GetComponent<PlayerCameraTether>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+           playerMovement.enabled = false;
+           playerCameraTether.enabled = false;
+        }
+    }
+
+}
