d16326df4ea9333dff26deba3932602e48161aa2
Alex Carolan <alexcarolan@alexs-mbp.home>
2021-01-04 15:58:30 +0000

    Added endless level generation

--- Mobile Game/Assets/Scripts/GenerateLevel.cs
+++ Mobile Game/Assets/Scripts/GenerateLevel.cs
@@ -6,35 +6,61 @@ public class GenerateLevel : MonoBehaviour
 {
 
     public int maxActiveParts = 15;
+    public int blockPrefabCount;
     public Transform startBlock;
 
     public Transform playerPosition;
 
     private float endPosition;
-    private float midPosition;
 
-    public GameObject test;
+    private GameObject[] blockPrefabs;
 
-    private GameObject[] blocks;
-
+    private int lastRandom;
 
     // Start is called before the first frame update
     void Start()
     {
-        endPosition = startBlock.localScale.z/2;
+        //Load block prefabs into array
+        blockPrefabs = new GameObject[blockPrefabCount];
 
+        for (int i=0; i<blockPrefabCount; i++)
+        {
+            string path = "Prefabs/Blocks/Block" + i;
+            blockPrefabs[i] = Resources.Load<GameObject>(path);
+        }
+
+        //Get offset from start block
+        endPosition = startBlock.localScale.z / 2;
+
+        //Generate inital blocks
         for (int i = 0; i < maxActiveParts; i++)
         {
             generateNewBlock();
         }
 
+        lastRandom = Random.Range(0, blockPrefabCount);
+
     }
 
     void generateNewBlock()
     {
-        endPosition = endPosition + test.GetComponent<Transform>().localScale.x;
-        Instantiate(test, new Vector3(0, 0, endPosition), Quaternion.identity);
-        endPosition = endPosition + test.GetComponent<Transform>().localScale.x;
+        //Generate randomnumber for prefab,avoid using same as last
+        int random = Random.Range(0, blockPrefabCount);
+        while (lastRandom == random)
+        {
+            random = Random.Range(0, blockPrefabCount);
+        }
+
+        lastRandom = random;
+
+        GameObject randomBlock = blockPrefabs[random];
+
+        endPosition = endPosition + (randomBlock.transform.Find("Floor").GetComponent<Transform>().localScale.z / 2);
+
+        Instantiate(randomBlock, new Vector3(0, 0, endPosition), Quaternion.identity);
+
+        endPosition = endPosition + (randomBlock.transform.Find("Floor").GetComponent<Transform>().localScale.z / 2);
+
     }
 
     // Update is called once per frame
